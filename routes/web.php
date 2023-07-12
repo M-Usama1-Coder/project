929109ad25c77324cf5446f0930bc1b89b125593
DevOP <wajahat.jarvistech@gmail.com>
2023-07-12 12:00:39 +0500

    tedsa

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::get('{path}/login', [AuthController::class, 'login'])->name('login');
 Route::get('signup', [AuthController::class, 'signup'])->name('signup');
 Route::post('register', [AuthController::class, 'register']);
 Route::post('auth', [AuthController::class, 'authenticate']);
